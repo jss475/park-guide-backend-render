c71abd838e1dc0702b62596bddc2bc58871c4eb3
jss475 <shin.seung.won@gmail.com>
2022-12-29 17:15:06 -0500

    allowed all on cors.rb as test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://park-guide-site.onrender.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
